40fc53b315f9cee902371fc977e7175d4f563488
apo-star <apolle.world@gmail.com>
2023-04-05 04:24:41 -0500

    Add title, modify navbar, add video blur

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,9 @@ export default function Home() {
         <video autoPlay muted loop className={styles.video}>
           <source src="/homePageBanner.mp4" type="video/mp4" />
         </video>
+        <div className={styles.title}>
+          <h1>Distribuidor de las mejores marcas</h1>
+        </div>
       </div>
       <div className="brands">brands</div>
       <div className="products">products</div>
