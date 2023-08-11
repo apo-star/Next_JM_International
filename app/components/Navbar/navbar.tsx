efd9f5ccd2f3ee05eadddae30cbe4912efddd332
apo-star <apolle.world@gmail.com>
2023-08-11 14:41:25 -0500

    Update navbar color burger icon

--- app/components/Navbar/navbar.tsx
+++ app/components/Navbar/navbar.tsx
@@ -82,7 +82,7 @@ const Navbar = ({ isHomePage }: { isHomePage: Boolean }) => {
           <IconButton
             onClick={onToggle}
             icon={
-              isOpen ? <CloseIcon w={3} h={3} /> : <HamburgerIcon w={5} h={5} />
+              isOpen ? <CloseIcon color={"white"} w={3} h={3} /> : <HamburgerIcon color={"white"} w={5} h={5} />
             }
             variant={"ghost"}
             aria-label={"Toggle Navigation"}
